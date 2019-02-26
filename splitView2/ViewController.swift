6ce9c28ca78d392e74798078c9bdb45af655b560
Anna Yuan <annayuan@annas-macbook-pro.local>
2019-02-26 09:04:10 -0500

    Initial Commit

---
+++ splitView2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  splitView2
+//
+//  Created by Anna Yuan on 2/26/19.
+//  Copyright © 2019 Anna Yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
